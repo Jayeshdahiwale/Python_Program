75087f0f026bf7dd2f1e9d5689dc64749f7dfed8
JAYESH <jayjdahivale@gmail.com>
2022-04-13 12:31:27 +0530

    [Jayesh]Modified the gambling function

--- FunctionalProgram/gambling.py
+++ FunctionalProgram/gambling.py
@@ -15,27 +15,28 @@ This function is tells whether the player will win or lose and the percentage of
 
 
 def gambling():
-    inputs = [int(i) for i in input("Enter the stake,bet,goal,no.fo cases separate by comma :").split(",")]
-    no_of_cases = inputs[3]
+    inputs = [int(i) for i in input("Enter the stake,bet,goal separate by comma :").split(",")]
     no_of_wins = 0
     no_of_loss = 0
-    for i in range(no_of_cases):
-        stake = inputs[0]
-        bet = inputs[1]
-        goal = inputs[2]
-        while stake >= bet:
-            result = random.randint(0, 1)
-            if result == 0:
-                stake -= bet
-            else:
-                stake += bet
-            if stake >= goal:
-                no_of_wins += 1
-                break
-            elif stake < bet:
-                no_of_loss += 1
-                break
-    print(f"Total no of wins {no_of_wins}")
+
+    stake = inputs[0]
+    bet = inputs[1]
+    goal = inputs[2]
+    while stake >= bet:
+        result = random.randint(0, 1)
+        if result == 0:
+            stake -= bet
+            no_of_loss += 1
+        else:
+            stake += bet
+            no_of_wins += 1
+        if stake >= goal:
+            print("Goal achieved")
+            break
+        elif stake < bet:
+            print("Gambler turns out to be bancrupt")
+            break
+    print(f"Total no of wins {no_of_wins} & Total no of loss {no_of_loss}")
     print(
         f"Percentage of win :{round(no_of_wins * 100 / (no_of_wins + no_of_loss), 2)}% ,Percentage of loss :{round(no_of_loss * 100 / (no_of_wins + no_of_loss), 2)}%")
 
